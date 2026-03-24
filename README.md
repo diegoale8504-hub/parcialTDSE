43fc4dea2d88ee37e243358a0f4165b0ff5231a7
unknown <diego.rozo-g@labinfo.is.escuelaing.edu.co>
2026-03-24 09:41:50 -0500

    Adelanto de lo que tengo

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # parcialTDSE
+Se debe de hacer una aplicacion we en la que tenemos dos servicios, uno de busqueda lineal y otro de busqueda binaria
+Por lo que tenemos que tener el servicio de busqueda que es en el que vamos a trabajar las request del user
---
+++ src/main/java/com/example/demo/searchService.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class searchService {
+
+    @GetMapping
+    public int linealSearch(@RequestParam int value){
+        for (int )
+    }
+}
